3f73b7df943e744f3071f10900bdc3264ee4bd21
Divanny <divannyjpm@gmail.com>
2025-02-12 15:52:29 -0400

    Updating projects and skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,10 @@ if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+} else {
+  // Set dark theme as default
+  document.body.classList.add(darkTheme)
+  themeButton.classList.add(iconTheme)
 }
 
 // Activate / deactivate the theme manually with the button
